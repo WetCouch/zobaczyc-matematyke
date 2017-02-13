b1a93d0f0c54c66cbb1dfec014b04be6cea0c0c3
Wojtek Janoszek <wetcouch@icloud.com>
2017-02-13 18:37:20 +0100

    Subsites jumbotron

---
+++ routes/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wetcouch on 13.02.2017.
+ */
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
